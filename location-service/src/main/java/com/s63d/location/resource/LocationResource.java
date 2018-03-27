254549c519376525651bd8d758094471e5261f46
Marvin <marvin.zwolsman@gmail.com>
2018-03-27 13:15:17 +0200

    Updated dingen door goos

--- location-service/src/main/java/com/s63d/location/resource/LocationResource.java
+++ location-service/src/main/java/com/s63d/location/resource/LocationResource.java
@@ -2,12 +2,13 @@ package com.s63d.location.resource;
 
 import com.s63d.generic.JsonResource;
 import com.s63d.location.domain.Location;
+import com.s63d.location.repository.LocationRepository;
 import com.s63d.location.service.LocationService;
 
 import javax.inject.Inject;
 
-public class LocationResource extends JsonResource<Location, Long, LocationService> {
+public class LocationResource extends JsonResource<Location, Long, LocationRepository, LocationService> {
     @Inject
     public LocationResource(LocationService service) { super(service); }
-    
+
 }
--- location-service/src/main/java/com/s63d/location/service/LocationService.java
+++ location-service/src/main/java/com/s63d/location/service/LocationService.java
@@ -2,6 +2,7 @@ package com.s63d.location.service;
 
 import com.s63d.generic.DomainService;
 import com.s63d.location.domain.Location;
+import com.s63d.location.repository.LocationRepository;
 
-public class LocationService extends DomainService<Location,Long> {
+public class LocationService extends DomainService<Location,Long, LocationRepository> {
 }
