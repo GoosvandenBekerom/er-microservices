bee5a53559302ffffb60a0f60e81c86bb4e7db25
Goos <goos.bekerom@gmail.com>
2018-03-27 11:57:01 +0200

    Added endpoint to get all ownerships of a user

--- account-service/src/main/java/com/s63d/account/resource/UserResource.java
+++ account-service/src/main/java/com/s63d/account/resource/UserResource.java
@@ -1,7 +1,11 @@
 package com.s63d.account.resource;
 
+import com.s63d.account.domain.Ownership;
+import com.s63d.account.domain.SimpleVehicle;
 import com.s63d.account.domain.User;
+import com.s63d.account.repository.UserRepository;
 import com.s63d.account.service.UserService;
+import com.s63d.annotation.Secured;
 import com.s63d.generic.JsonResource;
 
 import javax.inject.Inject;
@@ -9,9 +13,10 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("user")
-public class UserResource extends JsonResource<User, String, UserService> {
+public class UserResource extends JsonResource<User, Long, UserRepository, UserService> {
     @Inject
     public UserResource(UserService service) { super(service); }
 
@@ -29,5 +34,12 @@ public class UserResource extends JsonResource<User, String, UserService> {
         String token = service.loginUser(email, password);
         JsonObject json = Json.createObjectBuilder().add("token", token).build();
         return Response.ok(json).build();
+    }
+
+    @GET
+    @Secured({"police", "government", "admin"})
+    @Path("{id}/ownership")
+    public List<Ownership> getOwnerships(@PathParam("id") long id) {
+        return service.getOwnerships(id);
     }
 }
