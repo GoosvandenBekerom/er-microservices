559510d5ebb8bef7692f79e2b84c67fd4096465e
Goos <goos.bekerom@gmail.com>
2018-04-23 15:46:26 +0200

    Added vehicle service url

--- src/main/java/com/s63d/util/ServiceURLs.java
+++ src/main/java/com/s63d/util/ServiceURLs.java
@@ -2,4 +2,5 @@ package com.s63d.util;
 
 public class ServiceURLs {
     public final static String ACCOUNT_SERVICE = "http://account-service:8080/account-service/api";
+    public final static String VEHICLE_SERVICE = "http://vehicle-service:8080/vehicle-service/api";
 }
